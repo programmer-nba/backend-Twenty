7b1af73de1d261aa7d78e53a4118bb72ffc97e4d
Programmer NBA <programmer@nbadigitalworlds.com>
2024-08-15 19:00:24 +0700

    up

--- controllers/tossagun/artwork/artwork.controller.js
+++ controllers/tossagun/artwork/artwork.controller.js
@@ -6,7 +6,6 @@ module.exports.getProductAll = async (req, res) => {
 
 		const resp = await axios.post(`${process.env.TOSSAGUN_API}/api/api_service/artwork`, null, {
 			headers: {
-				// "Accept-Encoding": "gzip,deflate,compress",
 				"auth-token": process.env.TOSSAGUN_TOKEN
 			}
 		});
@@ -16,6 +15,26 @@ module.exports.getProductAll = async (req, res) => {
 		} else {
 			return res.status(403).send({ status: false, message: 'มีบางอย่างผิดพลาด' });
 		}
+	} catch (error) {
+		console.log(error)
+		return res.status(500).send({ message: "Internal Server Error" })
+	}
+};
+
+module.exports.getProductPrice = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const resp = await axios.post(`${process.env.TOSSAGUN_API}/api/api_service/artwork/price/${id}`, null, {
+			headers: {
+				"auth-token": process.env.TOSSAGUN_TOKEN
+			}
+		});
+		if (resp.data.status) {
+			return res.status(200).send({ status: true, message: 'ดึงข้อมูลสำเร็จ', data: resp.data.data })
+		} else {
+			return res.status(403).send({ status: false, message: 'มีบางอย่างผิดพลาด' });
+		}
+
 	} catch (error) {
 		console.log(error)
 		return res.status(500).send({ message: "Internal Server Error" })
--- routes/tossagun/artwork.js
+++ routes/tossagun/artwork.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const artwork = require("../../controllers/tossagun/artwork/artwork.controller");
 
 router.get("/", artwork.getProductAll);
+router.get("/price/:id", artwork.getProductPrice);
 
 module.exports = router;
